b69664304a8498b82bbc6739b9abf6a6eb48ff08
Thibault Duplessis <t@lichess.org>
2023-06-22 12:26:06 +0200

    pairing page wip

--- src/page/pairing.ts
+++ src/page/pairing.ts
@@ -4,6 +4,7 @@ import layout from '../view/layout';
 
 export class Pairing {
   constructor(readonly app: App) {}
-  render = () => layout(this.app, h('div', 'Pairing'));
   redraw = () => this.app.redraw(this.render());
+  render = () =>
+    layout(this.app, h('div.app-pairing', [h('h2', 'Pair two players'), h('p', 'Requires admin privileges.')]));
 }
