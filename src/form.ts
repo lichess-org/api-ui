59ef23136cce67f0658a14cbdecd1141071233dd
Thibault Duplessis <t@lichess.org>
2023-06-25 07:39:55 +0200

    refactor endpoints WIP

--- src/form.ts
+++ src/form.ts
@@ -1,3 +1,5 @@
+import { Redraw } from './interfaces';
+
 export interface Success<R> {
   result: R;
 }
@@ -19,3 +21,22 @@ export const formData = (data: any): FormData => {
   for (const k of Object.keys(data)) formData.append(k, data[k]);
   return formData;
 };
+
+export const formToEndpoint =
+  (form: FormData, redraw: () => void) =>
+  async <R>(f: (get: (key: string) => string) => Promise<Response>): Promise<Feedback<R>> => {
+    let feedback: Feedback<R>;
+    try {
+      const res = await f(key => form.get(key) as string);
+      const json = await res.json();
+      if (res.status != 200) throw json;
+      feedback = { result: json };
+    } catch (err) {
+      feedback = {
+        message: JSON.stringify(err),
+      };
+    }
+    redraw();
+    document.getElementById('endpoint-form')?.scrollIntoView({ behavior: 'smooth' });
+    return feedback;
+  };
